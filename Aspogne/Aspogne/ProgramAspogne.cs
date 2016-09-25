fad5296c1710a724c737ec0ab4c8a2711e861722
Nicolas G <nicolas.gilbert.contact@gmail.com>
2016-09-25 17:31:20 -0400

    Revert "Update project names + started code for Actuator, sensor and BDI"
    This reverts commit ae5c25f846fda74d28d0caf73ddbbc83273c5e23.

---
+++ Aspogne/Aspogne/ProgramAspogne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspogne
+{
+    public class ProgramAspogne
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
---
+++ Aspogne/MainProgram/ProgramMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspogne;
+using Environment;
+using System.Threading;
+
+namespace MainProgram
+{
+    class ProgramMain
+    {
+
+        static void Main(string[] args)
+        {
+            ProgramAspogne.Main();
+            //Environment.MainWindow window = new MainWindow();
+            Thread monthreadAspogne = new Thread(new ThreadStart(ProgramAspogne.Main));
+            //Thread monthreadEnvironment = new Thread(new ThreadStart(Environment.MainWindow.Run));
+
+            monthreadAspogne.Start();
+            Console.Read();
+        }
+    }
+}
